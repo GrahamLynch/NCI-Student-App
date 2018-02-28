67dd3ca5292a8288002311101d0eeb2abe0ddd83
Graham Lynch <grahamlynch10@hotmail.com>
2018-02-28 11:46:14 +0000

    Logout features added

--- app/src/main/java/com/example/graha/studentlogin/MainActivity.java
+++ app/src/main/java/com/example/graha/studentlogin/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.graha.studentlogin;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private Button Login;
     private int counter = 5;
     private TextView studentRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
 
     @Override
@@ -31,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
         studentRegistration = (TextView)findViewById(R.id.textViewRegister);
 
         Info.setText(" No of attempts remaining 5");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if (user != null){
+            finish();
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        }
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,18 +69,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void validate(String studentNumber, String password) {
-        if ((studentNumber.equals("x16120132")) && (password.equals("1234"))){
 
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
-        } else {
-            counter--;
+        progressDialog.setMessage("Logging into your Student Account");
+        progressDialog.show();
 
-            Info.setText("No of attempts remaining" + String.valueOf(counter));
 
-            if (counter == 0) {
-                Login.setEnabled(false);
+        firebaseAuth.signInWithEmailAndPassword(studentNumber, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Student Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                }else{
+                    Toast.makeText(MainActivity.this, "Student Login Failed", Toast.LENGTH_SHORT).show();
+                    counter--;
+                    Info.setText("No of attempts remaining" + counter);
+                    progressDialog.dismiss();
+                    if (counter == 0){
+                        Login.setEnabled(false);
+                    }
+                }
             }
-        }
+        });
     }
 }
